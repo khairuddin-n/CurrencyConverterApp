34e194ca587e809b3651663eb406efd6c18b342c
khairuddin-n <nastykhairuddin@gmail.com>
2023-04-06 20:54:31 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fromComboBox = new System.Windows.Forms.ComboBox();
+            this.toComboBox = new System.Windows.Forms.ComboBox();
+            this.fromLabel = new System.Windows.Forms.Label();
+            this.toLabel = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // fromComboBox
+            // 
+            this.fromComboBox.FormattingEnabled = true;
+            this.fromComboBox.Location = new System.Drawing.Point(78, 163);
+            this.fromComboBox.Name = "fromComboBox";
+            this.fromComboBox.Size = new System.Drawing.Size(208, 28);
+            this.fromComboBox.TabIndex = 0;
+            // 
+            // toComboBox
+            // 
+            this.toComboBox.FormattingEnabled = true;
+            this.toComboBox.Location = new System.Drawing.Point(415, 163);
+            this.toComboBox.Name = "toComboBox";
+            this.toComboBox.Size = new System.Drawing.Size(208, 28);
+            this.toComboBox.TabIndex = 1;
+            // 
+            // fromLabel
+            // 
+            this.fromLabel.AutoSize = true;
+            this.fromLabel.Location = new System.Drawing.Point(74, 140);
+            this.fromLabel.Name = "fromLabel";
+            this.fromLabel.Size = new System.Drawing.Size(46, 20);
+            this.fromLabel.TabIndex = 2;
+            this.fromLabel.Text = "From";
+            // 
+            // toLabel
+            // 
+            this.toLabel.AutoSize = true;
+            this.toLabel.Location = new System.Drawing.Point(415, 140);
+            this.toLabel.Name = "toLabel";
+            this.toLabel.Size = new System.Drawing.Size(27, 20);
+            this.toLabel.TabIndex = 3;
+            this.toLabel.Text = "To";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(78, 268);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(208, 26);
+            this.amountTextBox.TabIndex = 4;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(419, 268);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(204, 26);
+            this.resultTextBox.TabIndex = 5;
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Location = new System.Drawing.Point(74, 250);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(65, 20);
+            this.amountLabel.TabIndex = 6;
+            this.amountLabel.Text = "Amount";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(415, 245);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(55, 20);
+            this.resultLabel.TabIndex = 7;
+            this.resultLabel.Text = "Result";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertButton.AutoSize = true;
+            this.convertButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.convertButton.Location = new System.Drawing.Point(292, 332);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(121, 53);
+            this.convertButton.TabIndex = 8;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(702, 46);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Currency Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.toLabel);
+            this.Controls.Add(this.fromLabel);
+            this.Controls.Add(this.toComboBox);
+            this.Controls.Add(this.fromComboBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox fromComboBox;
+        private System.Windows.Forms.ComboBox toComboBox;
+        private System.Windows.Forms.Label fromLabel;
+        private System.Windows.Forms.Label toLabel;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        private const string API_KEY = "e84200f83114407da71f75603c844683"; 
+        private const string API_URL = "https://openexchangerates.org/api/latest.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Tambahkan pilihan mata uang pada comboBox "From"
+            fromComboBox.Items.Add("USD");
+            fromComboBox.Items.Add("EUR");
+            fromComboBox.Items.Add("GBP");
+            fromComboBox.Items.Add("JPY");
+            fromComboBox.Items.Add("SGD");
+            fromComboBox.Items.Add("IDR");
+
+            // Tambahkan pilihan mata uang pada comboBox "To"
+            toComboBox.Items.Add("USD");
+            toComboBox.Items.Add("EUR");
+            toComboBox.Items.Add("GBP");
+            toComboBox.Items.Add("JPY");
+            toComboBox.Items.Add("SGD");
+            toComboBox.Items.Add("IDR");
+
+            // Set default mata uang pada comboBox "From" dan "To"
+            fromComboBox.SelectedItem = "USD";
+            toComboBox.SelectedItem = "IDR";
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            string fromCurrency = fromComboBox.SelectedItem.ToString();
+            string toCurrency = toComboBox.SelectedItem.ToString();
+            double amount = double.Parse(amountTextBox.Text);
+
+            string url = $"{API_URL}?app_id={API_KEY}&base={fromCurrency}&symbols={toCurrency}";
+
+            using (var client = new WebClient())
+            {
+                try
+                {
+                    string response = client.DownloadString(url);
+                    JObject json = JObject.Parse(response);
+                    double exchangeRate = (double)json["rates"][toCurrency];
+                    double result = amount * exchangeRate;
+
+                    resultTextBox.Text = result.ToString("0.00");
+                }
+                catch (WebException ex)
+                {
+                    MessageBox.Show("Failed to connect to the API server. Please check your internet connection and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error has occurred while converting the currency. Please try again later.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+        }
+    }
+}
